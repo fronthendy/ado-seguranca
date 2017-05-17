8fc3c7a7463b04868b6a976097a5321118a3e882
HENDY ALMEIDA DE SOUZA PEREIRA <hendy.aspereira1@senacsp.edu.br>
2017-05-16 21:58:50 -0300

    sql novo

--- news-samurai.sql
+++ news-samurai.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 10-Maio-2017 às 03:45
+-- Generation Time: 17-Maio-2017 às 02:58
 -- Versão do servidor: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
@@ -55,6 +55,24 @@ CREATE TABLE `likes` (
   `data` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Extraindo dados da tabela `likes`
+--
+
+INSERT INTO `likes` (`id_like`, `id_usuario`, `data`) VALUES
+(1, 0, '2017-05-17 00:05:24'),
+(2, 0, '2017-05-17 00:06:16'),
+(3, 1, '2017-05-17 00:45:25'),
+(4, 1, '2017-05-17 00:45:27'),
+(5, 1, '2017-05-17 00:45:27'),
+(6, 1, '2017-05-17 00:45:27'),
+(7, 1, '2017-05-17 00:45:28'),
+(8, 1, '2017-05-17 00:45:28'),
+(9, 1, '2017-05-17 00:45:40'),
+(10, 1, '2017-05-17 00:45:41'),
+(11, 1, '2017-05-17 00:45:41'),
+(12, 1, '2017-05-17 00:45:41');
+
 -- --------------------------------------------------------
 
 --
@@ -76,6 +94,25 @@ CREATE TABLE `noticias` (
 INSERT INTO `noticias` (`id_noticia`, `titulo`, `descricao`, `autor`, `data`) VALUES
 (4, 'De onde surgiu a palavra meme?', 'É criação do biólogo britânico Richard Dawkins, no livro O Gene Egoísta. A intenção era batizar uma unidade de informação cultural – assim como gene é uma unidade de informação genética. Exemplos: uma melodia assobiada que se espalha pelo escritório, um jeito de usar boné que vira moda na escola ou aquele vídeo que você e mais meio milhão curtiram e compartilharam no Facebook.', 'Hendy', '2017-05-09 23:21:51'),
 (5, 'Como tuíte de adolescente sobre frango empanado se tornou o mais compartilhado da história', 'O pedido de Carter Wilkerson quebrou o recorde que até então pertencia à apresentadora americana Ellen DeGeneres ao passar de 3,5 milhões de retuítes nesta terça-feira. <br>\r\nTudo começou em 5 de abril, quando o jovem americano, publicou um tuíte direcionado à empresa Wendy, perguntando quantos retuítes seriam necessários para que ele pudesse receber um ano de nuggets de frango de graça.', 'BBC', '2017-05-10 00:47:17');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nome_usuario` varchar(20) NOT NULL,
+  `senha` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nome_usuario`, `senha`) VALUES
+(1, 'admin', 'pipoca');
 
 --
 -- Indexes for dumped tables
@@ -102,6 +139,12 @@ ALTER TABLE `noticias`
   ADD PRIMARY KEY (`id_noticia`);
 
 --
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -114,12 +157,17 @@ ALTER TABLE `comentarios`
 -- AUTO_INCREMENT for table `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `id_like` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_like` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `noticias`
 --
 ALTER TABLE `noticias`
   MODIFY `id_noticia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- Constraints for dumped tables
 --
